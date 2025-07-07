d7a2aa554f57fd22880baa42803a0bafaa955692
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2025-07-07 15:58:48 -0500

    Terraform code ec2

---
+++ ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-06965018c3ac2742a"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "mongo" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-06965018c3ac2742a"]
+
+  tags = {
+    Name = "mongo"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-06965018c3ac2742a"]
+
+  tags = {
+    Name = "catalogue"
+  }
+}
