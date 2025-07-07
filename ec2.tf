ad9c904aae7c98912392f003029d1c30be46e070
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2025-07-07 16:18:01 -0500

    Terraform code DNS records updated

--- ec2.tf
+++ ec2.tf
@@ -8,6 +8,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z01256903NP1ETIISC009"
+  name    = "frontend.dev.idevops25.online"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongo" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -18,6 +26,14 @@ resource "aws_instance" "mongo" {
   }
 }
 
+resource "aws_route53_record" "mongo" {
+  zone_id = "Z01256903NP1ETIISC009"
+  name    = "mongo.dev.idevops25.online"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.mongo.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -26,4 +42,12 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue"
   }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z01256903NP1ETIISC009"
+  name    = "catalogue.dev.idevops25.online"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.catalogue.private_ip]
 }
